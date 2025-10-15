62999cdb71cac29f0f03bc4fc7f9ec3a5c163004
abdulwedud33 <abdulwedudyassin2@gmail.com>
2025-10-15 11:31:52 +0300

    last commit

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       sameSite: "none",
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
