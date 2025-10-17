376ab0e06f2fa8081dfe58b92d72e55a0bc7609d
abdulwedud33 <abdulwedudyassin2@gmail.com>
2025-10-17 20:22:23 +0300

    suspense added commit

--- frontend/app/(signin)/signIn/page.tsx
+++ frontend/app/(signin)/signIn/page.tsx
@@ -1,9 +1,16 @@
+import { Suspense } from 'react';
 import SignInComp from "@/components/SignInComp";
 
-export default async function SignInPage() {
+export default function SignInPage() {
   return (
     <section className="flex flex-col items-center justify-center">
-      <SignInComp />
+      <Suspense fallback={
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-green-800"></div>
+        </div>
+      }>
+        <SignInComp />
+      </Suspense>
     </section>
   );
 }
